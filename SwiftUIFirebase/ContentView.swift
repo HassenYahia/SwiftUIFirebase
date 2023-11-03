ef21c95a8088634d370ee99cfbad3325330c2d7b
HassenYahia <hassenabdurahman4047@gmail.com>
2023-11-02 22:30:37 -0400

    Initial Commit

---
+++ SwiftUIFirebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFirebase
+//
+//  Created by Hassen Abdurahman Yahia on 2023-11-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIFirebase/SwiftUIFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseApp.swift
+//  SwiftUIFirebase
+//
+//  Created by Hassen Abdurahman Yahia on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
